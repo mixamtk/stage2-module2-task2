fccffe4144576138c081059bb75c41242f66c74d
Mikhail Konchyts <mixamtk@gmail.com>
2023-06-02 16:49:54 +0300

    attempt 5)

--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -14,7 +14,7 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession httpSession = request.getSession(false);
-        if (httpSession == null || httpSession.getAttribute("user") == null) {
+        if (httpSession != null && httpSession.getAttribute("user") != null) {
             response.sendRedirect(request.getContextPath() + "/login.jsp");
         } else {
             response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
@@ -25,7 +25,7 @@ public class LoginServlet extends HttpServlet {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
         boolean isUser = Users.getInstance().getUsers().stream().anyMatch(login::equals);
-        if (isUser && password !=null ) {
+        if (isUser & password != null ) {
             HttpSession httpSession = request.getSession();
             httpSession.setAttribute("user", login);
             httpSession.setAttribute("password", password);
